d0b966b299c50492e1094fb3bbf779e32ab98fc6
Florian da Costa <florian.dacosta@akretion.com>
2022-02-01 18:09:36 +0100

    [CUS] FIX MO state when all raw material is consumed but some workorders are still pending

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -474,7 +474,11 @@ class MrpProduction(models.Model):
                 production.state = 'draft'
             elif all(move.state == 'cancel' for move in production.move_raw_ids):
                 production.state = 'cancel'
-            elif all(move.state in ('cancel', 'done') for move in production.move_raw_ids):
+            # Now that we really consume at operation, native state may be False
+            # if last workorder does not consume anything (assembling for instance)
+            # state will go to done because all raw materials are consumed, even though
+            # there are pending operations and the finished move is not done.
+            elif all(move.state in ('cancel', 'done') for move in production.move_raw_ids) and all(wo_state in ('done', 'cancel') for wo_state in production.workorder_ids.mapped('state')):
                 production.state = 'done'
             elif production.workorder_ids and all(wo_state in ('done', 'cancel') for wo_state in production.workorder_ids.mapped('state')):
                 production.state = 'to_close'
